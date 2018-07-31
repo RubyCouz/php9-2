dbb3c68dccecf99d0656dd170ff2f683d0ea1fb5
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 16:29:44 +0200

    exo2

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <p class="date">
+      <span id="blink">
+      Nous sommes le <?php echo date('d - m - y'); ?>
+  </span>
+ </p>
+   <script src="assets/js/script.js" charset="utf-8"></script>
+  </body>
+</html>
